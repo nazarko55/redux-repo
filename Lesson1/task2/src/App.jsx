f4b8f2bdd9b678769c5ac6934fe40ca21e72fdf8
Nazar <sikirinskijn@gmailcom>
2020-10-28 13:38:49 +0200

    react-context user

---
+++ Lesson1/task2/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import { DataContext } from "./context";
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: "Nikola Tesla",
+      avatar_url: "https://avatars3.githubusercontent.com/u10001",
+    },
+  };
+  render() {
+    return (
+      <div>
+        <DataContext.Provider value={this.state.userData}>
+          <Header />
+        </DataContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Lesson1/task2/src/context.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const state = {
+  name: "Nikola Tesla",
+  avatar_url: "https://avatars3.githubusercontent.com/u10001",
+};
+
+export const DataContext = React.createContext();
