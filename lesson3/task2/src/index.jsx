2711a3a8ac94fcfca6f793af9b853bc7cad55b25
Nazar <sikirinskijn@gmailcom>
2020-11-02 15:01:25 +0200

    redux dev-tools

---
+++ lesson3/task2/src/index.jsx
@@ -0,0 +1,22 @@
+// import store from './store';
+// import { addUser, deleteUser, updateUser } from './users.actions';
+// import { increment, decrement, reset } from './counter.actions';
+
+// store.subscribe(() => console.log(store.getState()))
+
+// // store.dispatch(addUser({ id: 76, name: 'Sarah' }));
+// // store.dispatch(addUser({ id: 15, name: 'Alex' }));
+
+// // store.dispatch(deleteUser(15));
+// // console.log(store.getState())
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(decrement())
+// store.dispatch(reset())
+
+// store.dispatch(addUser({ id: 1, name: 'John' }));
+// store.dispatch(addUser({ id: 14, name: 'Bob' }));
+
+// store.dispatch(deleteUser(14));
+// store.dispatch(updateUser(1, { id: 56, name: 'Jane', age: 23 }));
