eb331d4b403614156957d62549a621a6387169b2
Nazar <sikirinskijn@gmailcom>
2020-11-03 16:18:10 +0200

    react-redux mapDispatch connector

---
+++ lesson4/task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from '../components/Counter';
+import { Provider } from 'react-redux';
+import store from './store';
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  )
+};
+
+export default App;
